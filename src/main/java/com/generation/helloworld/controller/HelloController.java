b8bb12fa3c9dad365b32d0c88467ae09e1016fa3
status <Status>
2023-12-07 18:03:27 -0300

    Spring

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -11,12 +11,42 @@ public class HelloController {
 
 	@GetMapping
 	private String hello() {
-		return "<b>Hello Turma 68!!</b> <br /> Segunda Frase";
+		return "<i>Hello Word !!</i> <br /> By: Matheus Soares Rodrigues";
 	}
 	
-	@GetMapping("/top")
-	private String top() {
-		return "<i>\n Turma 68 é TOP!!</i>";
+	@GetMapping("/bsm")
+	private String bsm() {
+		return "<b>\n BSM </b> "
+				+ "<br /> • Orientação ao futuro.\r\n"
+				+ "<br /> • Responsabilidade pessoal.\r\n"
+				+ "<br /> • Mentalidade de crescimento. \r\n"
+				+ "<br /> • Persistência.\r\n"
+				+ "<br /> • Trabalho em equipe. \r\n"
+				+ "<br /> • Orientação aos detalhes.\r\n"
+				+ "<br /> • Proatividade e comunicação.\r\n"
+				+ "<br /> • Boas Praticas.\r\n"
+				+ "<br /> • Cultura de feedbacks.";
+	}
+	
+	@GetMapping("/objetivos")
+	private String objetivos() {
+		return "<b>\n Objetivos da Semana !! </b> "
+				+ "<br /> Para essa semana eu tenho o objetivo"
+				+ "<br /> de me aprimorar nas BSMs voltadas a "
+				+ "<br /> comunicação e abrir a camera com mais"
+				+ "<br /> frequencia, para assim me tornar um pro-,"
+				+ "<br /> fissional com cada vez mais qualidades,"
+				+ "<br /> e assim, me tornar um membro de equipe"
+				+ "<br /> cada vez mais útil para a equipe como "
+				+ "<br /> um todo."
+				+ "<br /> "
+				+ "<br /> <i>\n Metas Tecnicas</i>"
+				+ "<br /> Insomnia"
+				+ "<br /> Spring"
+				+ "<br /> Logica de programação"
+				+ "<br /> SQL"
+				+ "<br /> DER"
+				+ "<br /> Java";
 	}
 	
 }
